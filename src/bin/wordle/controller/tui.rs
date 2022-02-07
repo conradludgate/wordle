f5629cfa52666032d899a9fac6a1b61b4029ffd7
Conrad Ludgate <conrad.ludgate@truelayer.com>
2022-02-07 21:25:26 +0000

    big ol refactor

---
+++ src/bin/wordle/controller/tui.rs
@@ -0,0 +1,133 @@
+use std::io::{self, Write};
+
+use cl_wordle::{
+    game::{Game, GameShare},
+    state::GuessError,
+    Match,
+};
+use crossterm::{
+    cursor,
+    event::{self, KeyCode},
+    execute,
+    terminal::{Clear, ClearType},
+};
+use eyre::Result;
+use owo_colors::{colors::Red, OwoColorize};
+
+mod guess;
+mod keyboard;
+mod letters;
+mod terminal;
+
+use self::{guess::Guesses, keyboard::Keyboard, letters::WordMatch, terminal::Terminal};
+
+pub struct Controller {
+    game: Game,
+    keyboard: Keyboard,
+    stdout: Terminal,
+}
+
+impl Controller {
+    pub fn new(game: Game) -> Result<Self> {
+        Ok(Self {
+            game,
+            keyboard: Keyboard::default(),
+            stdout: Terminal::new()?,
+        })
+    }
+
+    pub fn run(mut self) -> Result<Option<GameShare>> {
+        self.display_window()?;
+
+        let mut word = String::with_capacity(5);
+
+        let win = loop {
+            self.stdout.flush()?;
+            if let event::Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Esc => return Ok(None),
+                    KeyCode::Char(c) if c.is_ascii_alphabetic() && word.len() < 5 => {
+                        let c = c.to_ascii_lowercase();
+                        write!(self.stdout, "{}", c.to_ascii_uppercase())?;
+                        word.push(c);
+                    }
+                    KeyCode::Enter if word.len() == 5 => match self.guess(&*word) {
+                        Ok(()) => {
+                            self.display_window()?;
+
+                            if let Some(win) = self.game.state().game_over() {
+                                break win;
+                            }
+
+                            word.clear();
+                        }
+                        Err(_) => self.display_invalid(&word)?,
+                    },
+                    KeyCode::Backspace => {
+                        word.pop();
+                        write!(self.stdout, "{back} {back}", back = cursor::MoveLeft(1))?;
+                    }
+                    _ => {}
+                }
+            }
+        };
+
+        if !win {
+            self.write_final_solution()?;
+        }
+
+        execute!(self.stdout, cursor::Hide)?;
+
+        loop {
+            self.stdout.flush()?;
+            if let event::Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Enter | KeyCode::Esc => break,
+                    _ => {}
+                }
+            }
+        }
+
+        Ok(Some(self.game.share()))
+    }
+
+    fn guess(&mut self, word: &str) -> Result<(), GuessError> {
+        let matches = self.game.state_mut().guess(word)?;
+        self.keyboard.push(word, matches);
+        Ok(())
+    }
+
+    pub fn write_final_solution(&mut self) -> io::Result<()> {
+        write!(self.stdout, "{}", cursor::MoveDown(1))?;
+        write!(
+            self.stdout,
+            "{}",
+            WordMatch(self.game.state().solution(), Match::Exact)
+        )?;
+        write!(self.stdout, "{}", cursor::MoveTo(0, 10))
+    }
+
+    fn display_invalid(&mut self, invalid: &str) -> io::Result<()> {
+        self.display_window()?;
+        write!(self.stdout, "{}", invalid.to_ascii_uppercase().bg::<Red>())
+    }
+
+    fn display_window(&mut self) -> io::Result<()> {
+        let (_width, height) =
+            crossterm::terminal::size().map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+        write!(
+            self.stdout,
+            "{clear_all}{bottom_left}Press ESC to exit.{top_left}Wordle {game_type}{down}{keyboard}{state}",
+            clear_all = Clear(ClearType::All),
+            bottom_left = cursor::MoveTo(0, height-1),
+            top_left = cursor::MoveTo(0, 0),
+            game_type = self.game.game_type(),
+            down = cursor::MoveTo(0, 2),
+            keyboard = self.keyboard,
+            state = Guesses::from(self.game.state()),
+        )?;
+
+        Ok(())
+    }
+}
---
+++ src/game.rs
@@ -0,0 +1,87 @@
+use std::fmt;
+
+use crate::state::State;
+use eyre::{ensure, Result};
+
+pub struct Game {
+    state: State,
+    game_type: GameType,
+}
+
+impl Game {
+    #[cfg(feature = "time")]
+    pub fn new() -> Result<Self> {
+        use eyre::WrapErr;
+        let now =
+            time::OffsetDateTime::now_local().wrap_err("could not determine local timezone")?;
+        Self::from_date(now.date())
+    }
+
+    pub fn custom(solution: String) -> Result<Self> {
+        Self::new_raw(solution, GameType::Custom)
+    }
+
+    #[cfg(feature = "time")]
+    pub fn from_date(date: time::Date) -> Result<Self> {
+        let day = crate::get_day(date);
+        Self::from_day(day)
+    }
+
+    pub fn from_day(day: usize) -> Result<Self> {
+        let solution = crate::get_solution(day).to_owned();
+        Self::new_raw(solution, GameType::Daily(day))
+    }
+
+    fn new_raw(solution: String, game_type: GameType) -> Result<Self> {
+        ensure!(
+            crate::words::FINAL.contains(&&*solution),
+            "{} is not a valid solution",
+            solution
+        );
+        Ok(Self {
+            state: State::new(solution),
+            game_type,
+        })
+    }
+
+    pub fn game_type(&self) -> GameType {
+        self.game_type
+    }
+
+    pub fn state(&self) -> &State {
+        &self.state
+    }
+
+    pub fn state_mut(&mut self) -> &mut State {
+        &mut self.state
+    }
+
+    pub fn share(self) -> GameShare {
+        GameShare(self)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum GameType {
+    Daily(usize),
+    Custom,
+}
+
+impl fmt::Display for GameType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            GameType::Daily(day) => write!(f, "{}", day),
+            GameType::Custom => write!(f, "custom"),
+        }
+    }
+}
+
+pub struct GameShare(Game);
+
+impl fmt::Display for GameShare {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Wordle {game_type} ", game_type = self.0.game_type())?;
+        self.0.state().display_score_card(f)?;
+        Ok(())
+    }
+}
